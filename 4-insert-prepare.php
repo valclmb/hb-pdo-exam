c9570629eba8cdd02f378bcafa644b8fc17e97ec
ValCLMB <valentin.coulomb2@gmail.com>
2021-02-01 14:16:45 +0100

    4

--- 4-insert-prepare.php
+++ 4-insert-prepare.php
@@ -7,5 +7,20 @@ $data = [
     'Bérets',
     'Chapka',
     'Cache-oreilles',
-    'Bandeaux',
+    'Bandeaux'
 ];
+
+
+  $sql = "INSERT INTO category(name) VALUES (:name)";
+    
+    $statement = $connection->prepare($sql);
+    
+    foreach ($data as $value) {
+        $statement->bindParam(':name', $value, PDO::PARAM_STR);
+
+        $isDone = $statement->execute();
+        
+        if (!$isDone) {
+            throw new Exception('Erreur lors de l\'insertion de la donnée : bonnet'.$value);
+        }
+    }
