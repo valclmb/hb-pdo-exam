bc75ebb5f2a5ed00a5e963365157ec22d0823adf
ValCLMB <valentin.coulomb2@gmail.com>
2021-02-01 13:08:55 +0100

    pdo-exam-1

--- includes/connect.php
+++ includes/connect.php
@@ -2,3 +2,12 @@
 
 include 'db.inc.php';
 
+$dsn = $dbDsn;
+$user = "$dbUser"; // Utilisateur par défaut
+$password = "$dbPass"; // Par défaut, pas de mot de passe sur Wamp
+
+try {
+    $connection = new PDO($dsn, $user, $password);
+} catch (PDOException $e) {
+    echo 'Connexion échouée : ' . $e->getMessage();
+}
