be9425a67362ce357a434998d17c9b044532a007
ValCLMB <valentin.coulomb2@gmail.com>
2021-02-01 14:07:42 +0100

    3

--- 3-select-direct.php
+++ 3-select-direct.php
@@ -2,7 +2,11 @@
 
 include 'includes/connect.php';
 
-$data = [];
+$sql = "SELECT * FROM product";
+    $statement = $connection->query($sql); // Récupération d'un objet PDOStatement
+    $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+$data = $results;
 ?>
 
 <table>
@@ -12,12 +16,28 @@ $data = [];
         <th>Prix</th>
         <th>En stock</th>
     </tr>
-    <?php foreach ($data as $beanie) { ?>
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-    <?php } ?>
+       <?php
+    foreach ($data as $beanie) {
+        echo '
+                <tr>
+                    <td>
+                        ' . $beanie['name'] . '
+                    </td>
+                    
+                    <td>
+                        ' . $beanie['description'] . '
+                    </td>
+                    
+                    <td>
+                        ' . $beanie['price'] . '
+                    </td> 
+                                   
+                    <td>
+                        ' . $beanie['stock'] . '
+                    </td>
+       
+                </tr>';
+    }
+    ?>
 </table>
+ 
